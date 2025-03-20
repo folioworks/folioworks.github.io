b85523fbc1db3cc12b975f46bdbcdc7d83353dcf
Pratik Kabra <pratik@kabra.com>
2025-03-20 14:19:04 -0500

    Set deployment branch to gh-pages

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -19,6 +19,7 @@ const config: Config = {
   // If you aren't using GitHub pages, you don't need these.
   organizationName: "folioworks", // Usually your GitHub org/user name.
   projectName: "folioworks.github.io", // Usually your repo name.
+  deploymentBranch: "gh-pages",
 
   onBrokenLinks: "throw",
   onBrokenMarkdownLinks: "warn",
